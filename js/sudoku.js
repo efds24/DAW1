f30af742bfdba32769a3395d6ed682f082cfdc82
nicofero01@gmail.com <nicofero01@gmail.com>
2023-03-31 15:00:49 +0200

    Sudoku finalmente acabado

--- js/sudoku.js
+++ js/sudoku.js
@@ -1,7 +1,7 @@
 const sudokuSize = 9;
-const board = Array.from({length: 9}, () => Array(9).fill(0));
+const board = Array.from({ length: 9 }, () => Array(9).fill(0));
 var casillaActual;
-let x=0;
+let x = 0;
 
 function renderBoard() {
   let table = document.getElementById("sudoku");
@@ -22,31 +22,48 @@ function renderBoard() {
       if ((i + 1) % 3 === 0 && i !== 8) {
         cell.classList.add('borde-abajo');
       }
-      cell.addEventListener('click',newNumber);
+      cell.addEventListener('click', newNumber);
       row.appendChild(cell);
     }
     table.appendChild(row);
   }
-  if(document.getElementById('victory')!=null){
+  if (document.getElementById('victory') != null) {
     document.getElementById('victory').remove();
   }
 }
 
-document.addEventListener('keydown',writeNumber);
+document.addEventListener('keydown', writeNumber);
 
-function newNumber(event){
-  if(x!=0)
-    casillaActual.classList.remove('casilla-elegida');  
+function newNumber(event) {
+  if (x != 0)
+    casillaActual.classList.remove('casilla-elegida');
   x++;
   casillaActual = event.target;
   casillaActual.classList.add('casilla-elegida');
 }
 
-function writeNumber(event){
-  if(!casillaActual.classList.contains("initial") && event.keyCode>48 && event.keyCode<58)
+function writeNumber(event) {
+
+  let tablero = document.getElementById('sudoku').children;
+  let x = 0;
+  let y = 0;
+  for (let i = 0; i < tablero.length; i++) {
+    let fila = tablero[i].children;
+    for (j = 0; j < fila.length; j++) {
+      if (fila[j].classList.contains('casilla-elegida')) {
+        x = i;
+        y = j;
+      }
+    }
+  }
+  if (!casillaActual.classList.contains("initial") && event.keyCode > 48 && event.keyCode < 58) {
     casillaActual.innerHTML = event.key;
-  if(!casillaActual.classList.contains("initial") && event.keyCode==8)
+    board[x][y] = Number(event.key);
+  }
+  if (!casillaActual.classList.contains("initial") && event.keyCode == 8) {
     casillaActual.innerHTML = "";
+    board[x][y] = 0;
+  }
 }
 
 function isValid(board, row, col, num) {
@@ -55,13 +72,13 @@ function isValid(board, row, col, num) {
     if (board[row][i] === num && i !== col) {
       return false;
     }
-  }  
+  }
   //Comprueba que no se repita el numero en la columna
   for (let i = 0; i < sudokuSize; i++) {
     if (board[i][col] === num && i !== row) {
       return false;
     }
-  }  
+  }
   //Comprueba que no se repita el numero en las cajas
   let boxRow = Math.floor(row / 3) * 3;
   let boxCol = Math.floor(col / 3) * 3;
@@ -76,38 +93,40 @@ function isValid(board, row, col, num) {
 }
 
 function checkSudoku() {
-  for (let i = 0; i < sudokuSize; i++) {
-    for (let j = 0; j < sudokuSize; j++) {
-      if (board[i][j] === 0 || !isValid(board, i, j, board[i][j])) {
-        alert("El sudoku es incorrecto :(");
-        return;
+  if (document.getElementById('victory') == null) {
+    for (let i = 0; i < sudokuSize; i++) {
+      for (let j = 0; j < sudokuSize; j++) {
+        if (board[i][j] === 0 || !isValid(board, i, j, board[i][j])) {
+          alert("El sudoku es incorrecto :(");
+          return;
+        }
       }
     }
+    let victory = document.createElement("h1");
+    victory.id = "victory";
+    victory.innerHTML = "SUDOKU CORRECTO";
+    document.querySelector("#center").before(victory);
   }
-  let victory = document.createElement("h1");
-  victory.id="victory";
-  victory.innerHTML = "SUDOKU CORRECTO";
-  document.querySelector("#center").before(victory);
 }
 
 
-function cargarTablero(fCallback){
+function cargarTablero(fCallback) {
   let xhttp = new XMLHttpRequest();
 
-  xhttp.onreadystatechange = function() {
+  xhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
       let xmlDoc = this.responseXML;
       let x = xmlDoc.getElementsByTagName("valor")
-      for (let i=0;i<x.length;i++){
-        board[Math.floor(i/9)][i%9] = Number(x[i].childNodes[0].nodeValue);
-        
+      for (let i = 0; i < x.length; i++) {
+        board[Math.floor(i / 9)][i % 9] = Number(x[i].childNodes[0].nodeValue);
+
       }
       fCallback();
     }
   };
-  
-  xhttp.open("GET","../ajax/sudoku.xml",true);
+
+  xhttp.open("GET", "../ajax/sudoku.xml", true);
   xhttp.send();
 }
 
-startGame= () =>  cargarTablero(renderBoard);
+startGame = () => cargarTablero(renderBoard);

852e8378acf128c355f8e2ec3a06ebcad8130fe3
nicofero01@gmail.com <nicofero01@gmail.com>
2023-03-31 16:45:47 +0200

    Acabado json

--- js/timeline.js
+++ js/timeline.js
@@ -1,26 +1,3 @@
-/*
-$(document).ready(function() {
-    $(window).scroll(function() {
-      var sectionTop = $('.container').offset().top;
-      var scrollTop = $(window).scrollTop();
-  
-      if (scrollTop >= sectionTop) {
-        var delay = 0;
-        $('.fade-in').each(function() {
-          var el = $(this);
-          setTimeout(function() {
-            el.parent().addClass('active');
-          }, delay);
-          delay += 1000;
-        });
-      } else {
-        $('.container').removeClass('active');
-        $('.container').css('height', 'auto');
-      }
-    });
-  });
-  */
-
 function leerJSON() {
   fetch('../ajax/timeline.json')
   .then(response => response.json())

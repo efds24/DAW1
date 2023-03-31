33fce79283288d28fca794046571dffec86f9939
nicofero01@gmail.com <nicofero01@gmail.com>
2023-03-31 16:37:27 +0200

    Creo que acabado

--- js/timeline.js
+++ js/timeline.js
@@ -26,10 +26,21 @@ function leerJSON() {
   .then(response => response.json())
   .then(json => {
     for(x in json.Evento){
-      console.log(json.Evento[x].Nombre);
       let bloque = document.createElement("p");
       bloque.innerHTML = json.Evento[x].Nombre;
-      document.querySelector("#juego").append(bloque);
+      document.querySelector("#bloques").append(bloque);
+    }
+  });
+}
+
+function orderBlocks(){
+  fetch('../ajax/timeline.json')
+  .then(response => response.json())
+  .then(json => {
+    let ordenados = json.Evento.sort(function(a, b) {return Number(a.Ano) - Number(b.Ano)});
+    for(x in ordenados){
+      let bloque = document.querySelector('#bloques');
+      bloque.childNodes[x].innerHTML = ordenados.Evento[x].Nombre;
     }
   });
 }
